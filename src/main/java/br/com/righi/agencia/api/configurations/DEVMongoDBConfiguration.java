4c7f1c85b40b92f96ed87bae3617933f6dd71d0b
Diego Righi <diego_righi@icloud.com>
2023-05-26 11:21:50 -0300

    Variavel de ambiente para Profile

---
+++ src/main/java/br/com/righi/agencia/api/configurations/DEVMongoDBConfiguration.java
@@ -0,0 +1,22 @@
+package br.com.righi.agencia.api.configurations;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Profile("dev")
+@Configuration
+public class DEVMongoDBConfiguration {
+	
+	@Value("${MONGODB_URI}")
+	private String mongoDbConnectionUri;
+
+	@Bean
+    public MongoTemplate mongoTemplate() {
+        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoDbConnectionUri));
+    }
+	
+}
